96d11b3682c58a80df911cc5723da8de530e891b
lhj502819@163.com <lhj502819@163.com>
2021-07-23 17:13:39 +0800

    FalseSharingDemo commit

--- src/main/java/cn/onenine/jvm/reference/WeakReferenceTest.java
+++ src/main/java/cn/onenine/jvm/reference/WeakReferenceTest.java
@@ -1,11 +1,15 @@
 package cn.onenine.jvm.reference;
 
-import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Description：测试弱引用，弱引用也是用来描述非必须对象的，比软引用的强度还弱一些，被弱引用关联的对象只能生存到下一次垃圾收集为止。
- *                  当垃圾收集器开始工作，无论当前内存是否足够，都会回收掉只被弱引用关联的对象，
+ * 弱引用也是用来描述非必须对象的，比软引用的强度还弱一些，被弱引用关联的对象只能生存到下一次垃圾收集为止。
+ * 当垃圾收集器开始工作，无论当前内存是否足够，都会回收掉只被弱引用关联的对象，
+ *
+ * 1、测试弱引用的生命周期
+ * 2、弱引用的作用
  *
  * @author li.hongjian
  * @email lhj502819@163.com
@@ -39,6 +43,23 @@ public class WeakReferenceTest {
          *   第一次GC后：null
          *   再次创建对象后：null]
          */
+
+
+        System.out.println("---------------------------一条优雅的分割线---------------------------");
+        /**
+         * 从上边的情况看弱引用的作用和强引用其实是一样的，只要不被强引用关联则就会被回收，那么JDK官方设计它的目的是什么呢？请先看下边的案例
+         */
+        Map<String,Object> cacheMap = new HashMap<>(3);
+        String key = "X";
+        cacheMap.put(key , "x");
+        /**
+         * 假设在某种条件下我发现某个key对应的value对象没用了，打算把这个对象抛弃，通过key=null后，
+         *    由于在Map底层有对这个对象的引用，因此也不能使GC回收掉对象
+         * 因此设计了WeakHashMap类，具体WeakHashMap是如果工作的，请大家自行查阅
+         */
+
+
+
     }
 
 }
