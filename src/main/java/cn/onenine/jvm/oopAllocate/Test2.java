ed9d94c06e9122f9d5d30ceafce483497fe74f64
李宏健 <lhj502819@163.com>
2021-08-10 22:30:31 +0800

    测试大对象直接在老年代分配

--- src/main/java/cn/onenine/jvm/oopAllocate/Test2.java
+++ src/main/java/cn/onenine/jvm/oopAllocate/Test2.java
@@ -14,6 +14,18 @@ public class Test2 {
     private static final int _1MB = 1024 * 1024;
 
     public static void main(String[] args) {
+        /**
+         * Heap
+         *  def new generation   total 9216K, used 1983K [0x00000000fec00000, 0x00000000ff600000, 0x00000000ff600000)
+         *   eden space 8192K,  24% used [0x00000000fec00000, 0x00000000fedeffc0, 0x00000000ff400000)
+         *   from space 1024K,   0% used [0x00000000ff400000, 0x00000000ff400000, 0x00000000ff500000)
+         *   to   space 1024K,   0% used [0x00000000ff500000, 0x00000000ff500000, 0x00000000ff600000)
+         *  tenured generation   total 10240K, used 4096K [0x00000000ff600000, 0x0000000100000000, 0x0000000100000000)
+         *          可以看到Old区被使用了4MB，就是allocation对象
+         *    the space 10240K,  40% used [0x00000000ff600000, 0x00000000ffa00010, 0x00000000ffa00200, 0x0000000100000000)
+         *  Metaspace       used 3133K, capacity 4496K, committed 4864K, reserved 1056768K
+         *   class space    used 343K, capacity 388K, committed 512K, reserved 1048576K
+         */
         testPretenyreSuzeThreshold();
     }
     public static void testPretenyreSuzeThreshold(){
