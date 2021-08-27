7e3fcf49a8d3be22fa1e604f4c8d42e7029d6667
lhj502819@163.com <lhj502819@163.com>
2021-08-27 15:51:20 +0800

    Test Class.isInstance

--- src/main/java/cn/onenine/jvm/classloader/ClazzTest.java
+++ src/main/java/cn/onenine/jvm/classloader/ClazzTest.java
@@ -12,6 +12,13 @@ public class ClazzTest {
          * 判断调用者Class是否为传入Class的父类或者当前类，是则返回true
          */
         System.out.println(Animal.class.isAssignableFrom(Bird.class));
+
+        /**
+         * 判断传入对象是否为调用者的实例
+         */
+        Bird bird = new Bird();
+        System.out.println(Bird.class.isInstance(bird));
+
     }
 }
 
@@ -21,5 +28,9 @@ class Animal{
 }
 
 class Bird extends Animal{
+
+}
+
+interface Fish{
 
 }
