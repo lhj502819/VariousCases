d051c4265b4f377105e3fea2bb2a3b4b91964931
lhj502819@163.com <lhj502819@163.com>
2021-08-27 15:41:42 +0800

    Test Class.isAssignableFrom

---
+++ src/main/java/cn/onenine/jvm/classloader/ClazzTest.java
@@ -0,0 +1,25 @@
+package cn.onenine.jvm.classloader;
+
+/**
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2021/8/27
+ */
+public class ClazzTest {
+
+    public static void main(String[] args) {
+        /**
+         * 判断调用者Class是否为传入Class的父类或者当前类，是则返回true
+         */
+        System.out.println(Animal.class.isAssignableFrom(Bird.class));
+    }
+}
+
+
+class Animal{
+
+}
+
+class Bird extends Animal{
+
+}
