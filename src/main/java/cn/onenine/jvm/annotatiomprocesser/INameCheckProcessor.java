9d26f5917c7feaaceea3eea5c08004fd24d7a8a5
李宏健 <lhj502819@163.com>
2021-09-04 13:23:53 +0800

    自定义注解处理器

---
+++ src/main/java/cn/onenine/jvm/annotatiomprocesser/INameCheckProcessor.java
@@ -0,0 +1,10 @@
+package cn.onenine.jvm.annotatiomprocesser;
+
+/**
+ * TODO
+ *
+ * @author lihongjian
+ * @since 2021/9/4
+ */
+public interface INameCheckProcessor {
+}
--- src/main/java/cn/onenine/jvm/annotatiomprocesser/NameCheckProcessor.java
+++ src/main/java/cn/onenine/jvm/annotatiomprocesser/NameCheckProcessor.java
@@ -1,10 +1,8 @@
 package cn.onenine.jvm.annotatiomprocesser;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
@@ -15,12 +13,26 @@ import java.util.Set;
  * @email lhj502819@163.com
  * @since 2021/9/3
  */
+//支持所有的注解类型
 @SupportedAnnotationTypes("*")
 //只支持JDK8的Java代码
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-public class NameCheckProcessor extends AbstractProcessor {
+public class NameCheckProcessor extends AbstractProcessor{
+    private NameChecker nameChecker;
+    @Override
+    public void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        nameChecker = new NameChecker(processingEnv);
+    }
+
+    /*** 对输入的语法树的各个节点进行名称检查 */
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        if (!roundEnv.processingOver()) {
+            for (Element element : roundEnv.getRootElements()) {
+                nameChecker.checkName(element);
+            }
+        }
         return false;
     }
 }
