c7d8b0ba6fef1f7d4e0c4895c7af71a1be24e576
李宏健 <lhj502819@163.com>
2021-09-12 10:03:21 +0800

    1、Method Inline 2、Object mem layout test

---
+++ src/main/java/cn/onenine/jvm/compiletor/MethodInlining.java
@@ -0,0 +1,24 @@
+package cn.onenine.jvm.compiletor;
+
+/**
+ * 方法内联
+ *
+ * @author lihongjian
+ * @since 2021/9/4
+ */
+public class MethodInlining {
+    public static void foo(Object obj){
+        if(obj == null){
+            System.out.println("do....");
+        }
+    }
+    public static void testInline(String[] args){
+        Object object = null;
+        foo(object);
+    }
+
+    public static void main(String[] args) throws Exception{
+        testInline(args);
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
