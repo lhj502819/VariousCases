04978886b8d27afd40863569b74f237d19a4e920
李宏健 <lhj502819@163.com>
2021-10-12 22:47:09 +0800

    提交DelimiterBasedFrameDecoder使用代码

---
+++ CaseForNetty/src/main/java/cn/znnine/netty/nio/netty/echo/EchoClientHandler.java
@@ -0,0 +1,38 @@
+package cn.znnine.netty.nio.netty.echo;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author lihongjian
+ * @since 2021/10/12
+ */
+public class EchoClientHandler extends ChannelHandlerAdapter {
+    private  int counter;
+
+    static final String ECHO_REQ = "Hi,lhj.$_";
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for(int i = 0 ; i < 10 ; i++){
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ CaseForNetty/src/main/java/cn/znnine/netty/nio/netty/echo/EchoServer.java
@@ -0,0 +1,71 @@
+package cn.znnine.netty.nio.netty.echo;
+
+import cn.znnine.netty.nio.netty.v1.NettyTimeServer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * DelimiterBasedFrameDecoder使用，自动完成以分隔符作为码流结束标志的消息的解码
+ *
+ * @author lihongjian
+ * @since 2021/10/12
+ */
+public class EchoServer {
+    public void bind(int port) throws Exception{
+        //配置服务端的NIO线程
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //使用“$_”作为分隔符
+                            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+                            //第一个参数1024表示单条消息的最大长度，当达到该条长度后仍然没有查找到分隔符，就抛出TooLongFrameException异常
+                            //  防止由于异常码流确实分隔符导致的内存溢出，这是Netty解码器的可靠性保护
+                            //第二个参数是分隔符缓冲对象，存的就是分隔符
+                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,delimiter));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            //优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length >0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //None  采用默认值
+            }
+        }
+        new EchoServer().bind(port);
+    }
+}
