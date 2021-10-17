9cea227f12044001cf75d2a3374c5fe01409d5e5
李宏健 <lhj502819@163.com>
2021-10-17 17:02:35 +0800

    FixedLengthFrameDecoder示例代码提交

---
+++ CaseForNetty/src/main/java/cn/znnine/netty/nio/netty/fixedlength/EchoServer.java
@@ -0,0 +1,66 @@
+package cn.znnine.netty.nio.netty.fixedlength;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * FixedLengthFrameDecoder是固定长度解码器，它能够按照指定的长度对消息进行自动解码，开发者不需要考虑TCP的粘包/拆包问题
+ *
+ * 利用FixedLengthFrameDecoder解码器，无论一次接收到多少数据报，它都会按照设置的固定长度进行解码，
+ *  如果是半包消息，FixedLengthFrameDecoder会缓存半包消息并等待下个包到达后进行拼包，直到读取到一个完整的包
+ *
+ * @author lihongjian
+ * @since 2021/10/17
+ */
+public class EchoServer {
+    public void bind(int port) throws Exception{
+        //配置服务端的NIO线程
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            //优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length >0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //None  采用默认值
+            }
+        }
+        new EchoServer().bind(port);
+    }
+}
---
+++ CaseForNetty/src/main/java/cn/znnine/netty/nio/netty/fixedlength/EchoServerHandler.java
@@ -0,0 +1,26 @@
+package cn.znnine.netty.nio.netty.fixedlength;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 负责将读取到的消息打印出来
+ *
+ * @author lihongjian
+ * @since 2021/10/17
+ */
+@Slf4j
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Receive client : [" + msg + " ]");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
