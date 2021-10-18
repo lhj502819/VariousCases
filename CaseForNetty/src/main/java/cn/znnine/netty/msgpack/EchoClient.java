d618fd7a61e6a0939f010918d2a93fa6a87ec324
李宏健 <lhj502819@163.com>
2021-10-18 23:11:07 +0800

    MessagePack编解码代码提交

---
+++ CaseForNetty/src/main/java/cn/znnine/netty/msgpack/EchoClient.java
@@ -0,0 +1,62 @@
+package cn.znnine.netty.msgpack;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @author lihongjian
+ * @since 2021/10/12
+ */
+public class EchoClient {
+    public void connect(int port, String host) throws Exception {
+        //配置客户端NIO线程组
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        /**
+                         * 当创建NioSocketChannel成功之后，在进行初始化时，将它的ChannelHandler设置到ChannelPipeline中
+                         * 用于处理网络I/O事件
+                         */
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            //发起异步连接操作
+            ChannelFuture f = b.connect(host, port).sync();
+
+            //等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            //优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                //None  采用默认值
+            }
+        }
+        new EchoClient().connect(port, "127.0.0.1");
+    }
+}
