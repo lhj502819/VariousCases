3e0ef7a124c4886f02194dbdebcc19d073bd1906
lhj502819 <lhj502819@163.com>
2022-09-16 16:32:23 +0800

    cf commit

---
+++ CaseForCompletableFuture/src/main/java/cn/onenine/cf/JdkCompletableFuture.java
@@ -0,0 +1,8 @@
+package cn.onenine.cf;/**
+ * Description：
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2022/9/16 15:55
+ */public class JdkCompletableFuture {
+}
---
+++ CaseForCompletableFuture/src/test/java/cn/onenine/cf/test/JdkCompletableFutureTest.java
@@ -0,0 +1,8 @@
+package cn.onenine.cf.test;/**
+ * Description：
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2022/9/16 16:12
+ */public class JdkCompletableFutureTest {
+}
